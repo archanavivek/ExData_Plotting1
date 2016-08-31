298a7e7c680c73e6c97dda605c92379391d242d8
archanavivek <archana.vivek@gmail.com>
2016-08-30 21:16:40 -0700

    Delete Plot3.R
    going to re-do

--- Plot3.R
+++
@@ -1,9 +0,0 @@
-png(filename='plot3.png',width=480,height=480,units='px')
-columnlines<-c("black","red","blue")
-labels<-c("Submetering 1","submetering 2","submetering 3")
-plot(power.consumption$Datetime,power.consumption$SubMetering1,type='l',col=columnlines[1],xlab='',ylab='Energy sub metering')
-lines(power.consumption$Datetime,power.consumption$SubMetering2,col=columnlines[2])
-lines(power.consumption$Datetime,power.consumption$SubMetering3,col=columnlines[3])
-# add legend
-legend('topright',legend=labels,col=columnlines,lty='solid')
-dev.off()
